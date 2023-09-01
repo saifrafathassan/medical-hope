e8f9905791afee3b373bf2ec4719a00a2329ec32
saif <saifrafat99@gmail.com>
2023-09-01 11:35:24 +0300

    lazy added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import Home from './routes/Home'
-import Services from './routes/Services'
+import React, { Suspense } from 'react';
 import {Route, Routes} from 'react-router-dom'
-import Blog from "./routes/Blog";
-import About from "./routes/About";
-import Contact from "./routes/Contact";
-import Details from "./routes/Details";
+const Home = React.lazy(() => import('./routes/Home'));
+const Services = React.lazy(() => import('./routes/Services'));
+const Blog = React.lazy(() => import('./routes/Blog'));
+const About = React.lazy(() => import('./routes/About'));
+const Contact = React.lazy(() => import('./routes/Contact'));
+const Details = React.lazy(() => import('./routes/Details'));
 
 
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>} scroll={() => window.scrollTo(0, 0)}/>
-      <Route path='/services' element={<Services/>} scroll={() => window.scrollTo(0, 0)}/>
-      <Route path='/blog' element={<Blog/>} scroll={() => window.scrollTo(0, 0)}/>
-      <Route path='/about' element={<About/>} scroll={() => window.scrollTo(0, 0)}/>
-      <Route path='/contact' element={<Contact/>} scroll={() => window.scrollTo(0, 0)}/>
-      <Route path='/details' element={<Details/>} scroll={() => window.scrollTo(0, 0)}/>
-    </Routes>
+    <Suspense fallback={<div className='loading'>Loading...</div>}>
+      <Routes>
+        <Route path='/' element={<Home/>} scroll={() => window.scrollTo(0, 0)}/>
+        <Route path='/services' element={<Services/>} scroll={() => window.scrollTo(0, 0)}/>
+        <Route path='/blog' element={<Blog/>} scroll={() => window.scrollTo(0, 0)}/>
+        <Route path='/about' element={<About/>} scroll={() => window.scrollTo(0, 0)}/>
+        <Route path='/contact' element={<Contact/>} scroll={() => window.scrollTo(0, 0)}/>
+        <Route path='/details' element={<Details/>} scroll={() => window.scrollTo(0, 0)}/>
+      </Routes>
+    </Suspense>
   );
 }
 
