1f905b9de1fc8ac4233c868b6a3c3726b47d0167
saif <saifrafat99@gmail.com>
2023-09-02 11:32:22 +0300

    contentpadding

--- src/components/Content.js
+++ src/components/Content.js
@@ -48,7 +48,7 @@ const Content = () => {
 
   return (
     <div className='md:px-0 px-3'>
-        <div className="content h-full w-full items-center flex flex-col md:flex-row justify-center gap-10 py-32 px-3">
+        <div className="content h-full w-full items-center flex flex-col md:flex-row justify-center gap-10 pt-6 pb-12 md:pb-20 px-3">
             <div className="flex justify-between w-full items-center border-b lg:w-[400px] ">
               <div className="pb-6">
                 <h1 className="text-2xl font-bold">DIAGNOSE</h1>
@@ -71,7 +71,7 @@ const Content = () => {
               <div className="pb-6 text-main"><FaWheelchair size={60}/></div>
             </div>
         </div>
-        <div ref={sectionRef} className='my-12 md:my-0 hero items-start flex flex-col h-screen justify-start ml-[315px]' style={{background: `url(${Img})`, backgroundPosition: 'center right', backgroundRepeat: 'no-repeat', opacity: isVisible ? 1 : 0, transform: isVisible ? 'translateY(0)' : 'translateY(60px)', transition: 'opacity 2s, transform 2s'}}>
+        <div ref={sectionRef} className='my-12 md:my-0 hero items-start flex flex-col h-screen justify-start ml-[290px]' style={{background: `url(${Img})`, backgroundPosition: 'center right', backgroundRepeat: 'no-repeat', opacity: isVisible ? 1 : 0, transform: isVisible ? 'translateY(0)' : 'translateY(60px)', transition: 'opacity 2s, transform 2s'}}>
             <div className='pl-2 lg:pl-6'>
                 <p className='text-[#999] pb-4 lg:pb-6'>WHY CHOOSE HOPE MEDICAL</p>
                 <h1 className='text-3xl lg:text-5xl font-light'><span className='text-main font-bold'>The Best</span> For Your Health</h1>
